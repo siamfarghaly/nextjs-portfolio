d1f97a6d82c080e87793e6a6defae8931d6fb4de
siamfarghaly <siam_farghaly@hotmail.be>
2023-03-14 15:18:49 +0100

    smaller social icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,11 +27,11 @@ export default function Home({
         <p>I'm a Webdeveloper & Digital creator focussed on front-end development, crossmedia design and online marketing.</p>
       </section>
       <section className={utilStyles.socials}>
-        <SocialIcon url="https://www.linkedin.com/in/siam-farghaly/" bgColor="#58a0f5" />
-        <SocialIcon url="https://www.github.com/siamfarghaly/" bgColor="#58a0f5" />
-        <SocialIcon url="https://www.instagram.com/siamfrghly/" bgColor="#58a0f5" />
-        <SocialIcon url="mailto:siam_farghaly@hotmail.be" bgColor="#58a0f5" />
-        <SocialIcon url="tel:0032494770327" bgColor="#58a0f5" />
+        <SocialIcon url="https://www.linkedin.com/in/siam-farghaly/" bgColor="#58a0f5" style={{height:40,width:40}}/>
+        <SocialIcon url="https://www.github.com/siamfarghaly/" bgColor="#58a0f5" style={{height:40,width:40}}/>
+        <SocialIcon url="https://www.instagram.com/siamfrghly/" bgColor="#58a0f5" style={{height:40,width:40}} />
+        <SocialIcon url="mailto:siam_farghaly@hotmail.be" bgColor="#58a0f5" style={{height:40,width:40}}/>
+        <SocialIcon url="tel:0032494770327" network="whatsapp" bgColor="#58a0f5" style={{height:40,width:40}}/>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>My Work</h2>
